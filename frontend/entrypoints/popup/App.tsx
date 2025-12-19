918925c6648ec9323c471e679d964c91ef1f7b11
Nicholas Hilbert <psnail273@gmail.com>
2025-12-18 22:36:32 -0600

    Moved everything to frontend folder

--- frontend/entrypoints/popup/App.tsx
+++ frontend/entrypoints/popup/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>WXT + React</h1>
+      <h1>WXT + React + Nick</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
